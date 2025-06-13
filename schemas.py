02a823fee207c1e5f924a1a8e9680b89238ae5a7
UKhAmster <vasonov04@gmail.com>
2025-06-13 22:26:18 +0300

    update

--- schemas.py
+++ schemas.py
@@ -14,7 +14,7 @@ class TrackOut(BaseModel):
 
 
 class ArtistOut(BaseModel):
-    id: int
+    id: Optional[int]
     name: str
     description_url: str
     photo_url: Optional[str]
@@ -42,7 +42,6 @@ class TrackOnly(BaseModel):
 
 
 class ArtistDetail(BaseModel):
-    id: int
     name: str
     description_url: str
     photo_url: Optional[str]
